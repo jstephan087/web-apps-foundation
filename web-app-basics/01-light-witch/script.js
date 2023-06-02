6b9150145c704ec811fb6cfb6b9044c954bacbf8
Julia Stephan <julia-stephan@hotmail.de>
2023-06-02 10:38:49 +0200

    fix: bg light switch bug

--- web-app-basics/01-light-witch/script.js
+++ web-app-basics/01-light-witch/script.js
@@ -3,13 +3,10 @@ const bdy = document.querySelector("body");
 
 btn.addEventListener("click", function () {
   btn.classList.toggle("toggle-border");
+  bdy.classList.toggle("toggle-bg");
   if (btn.classList.length === 2) {
     document.title = "Good Night";
   } else {
     document.title = "Good Morning";
   }
-});
-
-bdy.addEventListener("click", function () {
-  bdy.classList.toggle("toggle-bg");
 });
